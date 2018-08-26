ddcbc3c826565ae61f7937c0719fb0dc55fad151
teamnosleep1 <42708152+teamnosleep1@users.noreply.github.com>
2018-08-25 22:32:40 -0700

    Update WalletConfig.h

--- src/zedwallet/WalletConfig.h
+++ src/zedwallet/WalletConfig.h
@@ -10,10 +10,10 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string addressPrefix = "TRTL";
+    const std::string addressPrefix = "hEm1";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
-    const std::string ticker = "TRTL";
+    const std::string ticker = "HEM";
 
     /* The filename to output the CSV to in save_csv */
     const std::string csvFilename = "transactions.csv";
@@ -23,10 +23,10 @@ namespace WalletConfig
     const std::string addressBookFilename = ".addressBook.json";
 
     /* The name of your deamon */
-    const std::string daemonName = "TurtleCoind";
+    const std::string daemonName = "HemleyCoind";
 
     /* The name to call this wallet */
-    const std::string walletName = "zedwallet";
+    const std::string walletName = "Hemleywallet";
 
     /* The name of service/walletd, the programmatic rpc interface to a
        wallet */
